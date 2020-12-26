21c9f5b708ced98802b7eae567f2f1ca43b614f8
Jahangir Alam <100zinis@gmail.com>
2020-12-26 18:34:19 +0600

    Test
    demo sfml project...
    displays mouse location when pressed "z"

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::KeyPressed){
+				Vector2i globalposition = Mouse::getPosition(window);
+				if (event.key.code == Keyboard::Z){
+					cout << "the \"Z\" key was pressed... mouse location:" << endl;
+					cout << "x: " << globalposition.x << " y: " << globalposition.y << endl;
+				}
+			}
+            else if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
