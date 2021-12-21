a61e1995a25eb7c34e2c282b8c392616bf496553
Md. Jahangir Alam <70907172+x00jahangir@users.noreply.github.com>
2021-12-22 00:38:13 +0600

    User friendly build mechanism added
    # Added SFML directory in project
    # Switched platform to release
    # Changed respective flags and compiler settings

--- src/PRE_COMPILED_HEADER.h
+++ src/PRE_COMPILED_HEADER.h
@@ -1,10 +1,10 @@
 #ifndef pre_compiled_air321
 #define pre_compiled_air321
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+#include "SFML/Audio.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Network.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
 #include <iostream>
 #include <vector>
 #include <math.h>
